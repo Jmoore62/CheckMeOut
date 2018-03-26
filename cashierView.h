00dba9238c390f1ae2d18ca69e87b7debf2838cc
jacob moore <jmoore62@mail.csuchico.edu>
2018-03-26 21:21:36 +0100

    Add observable pattern

--- cashierView.h
+++ cashierView.h
@@ -1,12 +1,12 @@
 #ifndef _CASHIERVIEW_H_
 #define _CASHIERVIEW_H_
+
 #include "order.h"
 
 class cashierView
 {
   public:
     void displayRunningTotal(order running);
-
     void displayFinalReceipt(order final);
 };
 
